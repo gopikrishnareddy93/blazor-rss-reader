c91dbe8d789f57b3c07c8e62c6e80a37affb900c
Gopikrishna <gopi.jeevan97@gmail.com>
2018-08-27 23:07:13 -0500

    blazor-rss-reader

---
+++ src/BlazorRssReader/RssParser/RssType.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Toolkit.Parsers
+{
+    /// <summary>
+    /// Type of Rss.
+    /// </summary>
+    internal enum RssType
+    {
+        /// <summary>
+        /// Atom
+        /// </summary>
+        Atom,
+        /// <summary>
+        /// RSS
+        /// </summary>
+        Rss,
+        /// <summary>
+        /// Unknown
+        /// </summary>
+        Unknown
+    }
+}
---
+++ src/BlazorRssReader/RssParser/SchemaBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Toolkit.Parsers
+{
+    /// <summary>
+    /// Strong typed schema base class.
+    /// </summary>
+    public abstract class SchemaBase
+    {
+        /// <summary>
+        /// Gets or sets identifier for strong typed record.
+        /// </summary>
+        public string InternalID { get; set; }
+    }
+}
